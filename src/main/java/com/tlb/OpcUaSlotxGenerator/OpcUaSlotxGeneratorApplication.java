94ee2089bbe430db110b7c7c7ee6b59ea83e3bd9
Jakub Kusza <jakub.kuszabm@gmail.com>
2023-11-18 15:25:54 +0100

    #0 inital commit

---
+++ src/main/java/com/tlb/OpcUaSlotxGenerator/OpcUaSlotxGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.tlb.OpcUaSlotxGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpcUaSlotxGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpcUaSlotxGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tlb/OpcUaSlotxGenerator/OpcUaSlotxGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tlb.OpcUaSlotxGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpcUaSlotxGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
